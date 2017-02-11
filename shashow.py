89b5509a93089ae5eaaca2ad790cc5e57af8b474
Hrvoje T <hrvooje@gmail.com>
2017-02-11 01:03:05 +0100

    Changed init, added an icon, added confirmation with enter key, added sunken style form

--- shashow.py
+++ shashow.py
@@ -4,8 +4,9 @@
 import sys
 import crypt
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (QMainWindow, QLabel, QLineEdit, QPushButton,
-    QWidget, QApplication, QSystemTrayIcon, QFrame, QGridLayout)
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import (QLabel, QLineEdit, QPushButton,QWidget,
+    QApplication, QFrame, QGridLayout)
 
 
 
@@ -14,6 +15,15 @@ class MyApp(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
+        self.initui()
+
+
+    def initui(self):
+        # main window size, title and icon
+        self.setGeometry(300, 300, 800, 170)
+        self.setWindowTitle("Password hash calculator | Linux")
+        self.setWindowIcon(QIcon("shadow.png"))
+
         self.saltLabel = QLabel("Salt:")
         self.saltLine = QLineEdit()
         self.saltLine.setPlaceholderText("e.g. $6$xxxxxxxx")
@@ -21,17 +31,12 @@ class MyApp(QWidget):
         self.passwordLine = QLineEdit()
         self.hashLabel = QLabel("Hash:")
         self.hashSunkenLabel = QLabel()
-        # widget.hashSunkenLabel.setFrameStyle(QFrame.Box | QFrame.Sunken)
-        self.hashSunkenLabel.setFrameShadow(QFrame.Sunken)
+        # self.hashSunkenLabel.setFrameStyle(QFrame.Box | QFrame.Sunken)
+        self.hashSunkenLabel.setFrameStyle(QFrame.Panel | QFrame.Sunken)
+        # enable selectable text to be able to copy it
+        self.hashSunkenLabel.setTextInteractionFlags(Qt.TextSelectableByMouse)
         self.resultButton = QPushButton("&Calculate", self)
         self.resultButton.setMaximumSize(100, 50)
-
-        self.initui()
-
-    def initui(self):
-        # main window size, title and icon
-        self.setGeometry(300, 300, 500, 150)
-        self.setWindowTitle("Password hash calculator | Linux")
 
         # set layout
         grid = QGridLayout()
@@ -45,7 +50,16 @@ class MyApp(QWidget):
         self.setLayout(grid)
 
         self.resultButton.clicked.connect(self.logic)
+        # this method emits 'clicked' signal when 'Enter' is pressed
+        self.resultButton.setAutoDefault(True)
+        self.passwordLine.returnPressed.connect(self.logic)
+        self.saltLine.returnPressed.connect(self.logic)
 
+
+    # def show_about(self):
+    #
+    #     self.about = QWidget(self)
+    #     self.about.show()
 
     def logic(self):
         """
@@ -55,9 +69,17 @@ class MyApp(QWidget):
         password = self.passwordLine.text()
         resulting_hash = crypt.crypt(password, salt)
         self.hashSunkenLabel.setText(resulting_hash)
-        # sender = self.sender()
-        # print(sender)
-        # print(dir(MyApp))
+
+
+    def keyPressEvent(self, e):
+
+        if e.key() == Qt.Key_Return:
+            self.logic()
+
+    def keyPressEvent(self, e):
+
+        if e.key() == Qt.Key_Escape:
+            self.close()
 
 
 def main():
